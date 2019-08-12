5462c605f144410d45f7a1830a522bac25c49416
root <root@e2e-39-21.e2enetworks.net.in>
2019-08-12 18:06:35 +0530

    live to local

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,9 +1,9 @@
 <?php 
-define('BASE_URL','http://localhost/inventory/admin');
+define('BASE_URL','http://101.53.142.21/inventory/admin');
 // DB credentials.
 define('DB_HOST','mysql:dbname=management;host=localhost');
-define('DB_USER','suraj');
-define('DB_PASS','root1234');
+define('DB_USER','root');
+define('DB_PASS','root@1234');
 define('DB_NAME','management');
 // Establish database connection.
 try
